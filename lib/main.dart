a106191340d040a17b46e894b4a5e829c1b0d3b4
zonafar <zonafar2000@gmail.com>
2024-03-24 17:33:30 +0000

    stack and gestureDetector widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,55 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void userTap(){
+    print("Box is taped now");
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            // large box
-            Container(
-              color: Colors.deepPurple,
-              width: 200,
-              height: 200,
-            ),
-            // medium box
-            Container(
-              color: Colors.deepPurple[200],
-              width: 100,
-              height: 100,
-            ),
-        
-            //  small box
-            Container(
-              color: Colors.deepPurple[100],
-              width: 50,
-              height: 50,
-            )
-          ],
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              // large box
+              Container(
+                color: Colors.deepPurple,
+                width: 210,
+                height: 200,
+              ),
+              // medium box
+              Container(
+                color: Colors.deepPurple[200],
+                width: 110,
+                height: 100,
+              ),
+          
+              //  small box
+              GestureDetector(
+                onTap: userTap,
+                child: Container(
+                  color: Colors.deepPurple[100],
+                  width: 60,
+                  height: 50,
+                  child: Center(child: Text("Tap me", style: TextStyle(color: Colors.deepPurple, fontWeight: FontWeight.bold))),
+                ),
+              )
+            ],
+          ),
         ),
       )
       );
